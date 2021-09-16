ea16e900aac2e61d1d1aaeab4a74a7f09283d921
cdirks4 <cdirks4@me.com>
2021-09-16 14:34:35 -0400

    chart works for individual coins

---
+++ src/Chart/PortfolioChart.jsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from 'react';
+import { Doughnut, Line } from 'react-chartjs-2';
+import { getAuth } from 'firebase/auth';
+import * as portApi from '../apiCalls/portfolioCalls';
+import * as api from '../apiCalls/coingecko';
+
+const PortfolioChart = ({ portfolio, target }) => {
+	const [chartData, setChartData] = useState();
+	const [interval, setInterval] = useState('hourly');
+	const [days, setDays] = useState(1);
+	let usersMarketValue = null;
+	let marketValueObj = {};
+	let labels = [];
+
+	const getChartData = async (data) => {
+		for (let i = 0; i < data.length; i++) {
+			const res = await api.getCoinChartById(data[i].geckoId, days, interval);
+			usersMarketValue = await res.prices.map((price, j) => {
+				labels.push(`${i}hr`);
+				return { y: data[i].shares * price[1], x: price[0] };
+			});
+			marketValueObj[data[i].title] = usersMarketValue;
+		}
+
+		return marketValueObj;
+	};
+	console.log(portfolio);
+	useEffect(() => {
+		portfolio && getChartData(portfolio.coins).then((res) => setChartData(res));
+	}, [portfolio]);
+
+	return (
+		<>
+			<Line
+				data={{
+					labels:
+						chartData && [...Array(chartData[target].length).keys()].reverse(),
+					datasets: [
+						{
+							label: `${target} USD ${interval}`,
+							data: chartData && chartData[target],
+							borderColor: 'darkblue',
+							backgroundColor: 'rgba(100, 148, 230, 0.273)',
+							fill: true,
+							pointRadius: 0,
+						},
+					],
+					options: {
+						parsing: {
+							xAxisKey: 'x',
+							yAxisKey: 'y',
+						},
+					},
+				}}
+			/>
+		</>
+	);
+};
+
+export default PortfolioChart;
--- src/Portfolio/Portfolio.jsx
+++ src/Portfolio/Portfolio.jsx
@@ -4,70 +4,29 @@ import * as api from '../apiCalls/coingecko';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import { getAuth } from 'firebase/auth';
 import { Doughnut, Line } from 'react-chartjs-2';
+import PortfolioChart from '../Chart/PortfolioChart';
 const Portfolio = ({ currentUser, setCurrentUser }) => {
-	const [interval, setInterval] = useState('hourly');
-	const [days, setDays] = useState(1);
-	const [portfolio, setPortfolio] = useState();
+	const [portfolio, setPortfolio] = useState(null);
 	const [chartData, setChartData] = useState(null);
 	const [loading, setLoading] = useState(true);
+	const [target, setTarget] = useState('Bitcoin');
 	const auth = getAuth();
 
 	auth.onAuthStateChanged((user) => {
 		user ? setCurrentUser(user) : setCurrentUser(null);
 		console.log('hi');
 	});
-	let usersMarketValue = null;
-	let marketValueObj = {};
-	let labels = [];
-	const getChartData = async (data) => {
-		for (let i = 0; i < data.length; i++) {
-			const res = await api.getCoinChartById(data[i].geckoId, days, interval);
-			usersMarketValue = await res.prices.map((price, j) => {
-				labels.push(`${i}hr`);
-				return { y: data[i].shares * price[1], x: price[0] };
-			});
-			marketValueObj[data[i].title] = usersMarketValue;
-		}
-		setLoading(false);
-		return marketValueObj;
-	};
 
 	useEffect(() => {
 		currentUser &&
 			portApi.showPortfolio(currentUser.uid).then((res) => setPortfolio(res));
-		portfolio && getChartData(portfolio.coins).then((res) => setChartData(res));
-	}, []);
+	}, [currentUser]);
 
 	return !currentUser ? (
 		<h1>You must sign in to use this page</h1>
-	) : loading ? (
-		<p>loading</p>
 	) : (
 		<>
-			<Line
-				data={{
-					labels:
-						chartData &&
-						[...Array(chartData['Bitcoin'].length).keys()].reverse(),
-					datasets: [
-						{
-							label: `Market Value USD ${interval}`,
-							data: chartData && chartData['Bitcoin'],
-							borderColor: 'darkblue',
-							backgroundColor: 'rgba(100, 148, 230, 0.273)',
-							fill: true,
-							pointRadius: 0,
-						},
-					],
-					options: {
-						parsing: {
-							xAxisKey: 'x',
-							yAxisKey: 'y',
-						},
-					},
-				}}
-			/>
-
+			<PortfolioChart portfolio={portfolio} target={target} />
 			<Container className='border border-light mt-4 rounded'>
 				<Row className='border-bottom d-flex align-items-center'>
 					<Col></Col>
@@ -75,17 +34,10 @@ const Portfolio = ({ currentUser, setCurrentUser }) => {
 				{portfolio &&
 					portfolio.coins.map((coin) => {
 						return (
-							<Row>
+							<Row onClick={() => setTarget(coin.title)}>
 								<Col>{coin.title}</Col>
-								<Col>
-									{(
-										chartData &&
-										chartData[coin.title][0].y * coin.shares &&
-										coin.shares
-									).toFixed(3)}
-								</Col>
 								<Col>{coin.ppc}</Col>
-								<Col>{coin.shares.toFixed(3)}</Col>
+								<Col>{coin.shares && coin.shares.toFixed(3)}</Col>
 							</Row>
 						);
 					})}
